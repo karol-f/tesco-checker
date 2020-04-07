ff9530e41fa74013aa59ecb0f38dc72b05ad74e0
Karol Fabjańczuk <karol.fabjanczuk@gmail.com>
2020-04-07 14:54:13 +0200

    feat: do not recheck when found

--- script.sh
+++ script.sh
@@ -4,7 +4,6 @@ set -x
 
 rm -f dates*.png
 export DATES=
-export NEED_TO_RECHECK=
 echo "PREVIOUS_DATES $PREVIOUS_DATES"
 echo "LAST_FOUND_DATES_TIMESTAMP $LAST_FOUND_DATES_TIMESTAMP"
 echo "LAST_NOT_FOUND_DATES_TIMESTAMP $LAST_NOT_FOUND_DATES_TIMESTAMP"
@@ -16,25 +15,9 @@ if [ "$TEST_OUTPUT" == "Dates not changed" ] ; then
 elif [ "$TEST_OUTPUT" == "No available dates" ] ; then
     export LAST_NOT_FOUND_DATES_TIMESTAMP=$(date +%s)
 elif [[ ! -z "$TEST_OUTPUT" ]] ; then
-    echo '### Dates found, need to recheck ###'
-    export NEED_TO_RECHECK=true
-fi
-
-if [ "$NEED_TO_RECHECK" = true ] ; then
-  rm -f dates*.png
-  export DATES=
-
-  export TEST_OUTPUT=$(node test.js)
-
-  if [ "$TEST_OUTPUT" == "Dates not changed" ] ; then
-    export LAST_FOUND_DATES_TIMESTAMP=$(date +%s)
-  elif [ "$TEST_OUTPUT" == "No available dates" ] ; then
-      export LAST_NOT_FOUND_DATES_TIMESTAMP=$(date +%s)
-  elif [[ ! -z "$TEST_OUTPUT" ]] ; then
       echo '### Setting Dates ###'
       export DATES=$TEST_OUTPUT
       export PREVIOUS_DATES=$(node mergeDatesWithPrevious.js)
       export LAST_FOUND_DATES_TIMESTAMP=$(date +%s)
-  fi
 fi
 
